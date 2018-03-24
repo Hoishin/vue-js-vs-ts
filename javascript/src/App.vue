7584fad124a0f9a489649703701158cb544eeb63
Hoishin <hoishinxii@gmail.com>
2018-03-25 01:19:33 +0900

    style (javascript): html-indent

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <div id="app">
-    <header class="navbar">
-      <section class="navbar-section">
-        <a 
-          href="/" 
-          class="navbar-brand mr-2">TODO</a>
-        <a 
-          href="/" 
-          class="btn btn-link">Home</a>
-        <a 
-          href="/about" 
-          class="btn btn-link">About</a>
-        <a 
-          href="/list" 
-          class="btn btn-link">List</a>
-      </section>
-    </header>
+	<div id="app">
+		<header class="navbar">
+			<section class="navbar-section">
+				<a 
+					href="/" 
+					class="navbar-brand mr-2"
+				>TODO</a>
+				<a 
+					href="/" 
+					class="btn btn-link"
+				>Home</a>
+				<a 
+					href="/about" 
+					class="btn btn-link"
+				>About</a>
+				<a 
+					href="/list" 
+					class="btn btn-link"
+				>List</a>
+			</section>
+		</header>
 
-    <router-view/>>
-  </div>
+		<router-view/>>
+	</div>
 </template>
 
 <style>
