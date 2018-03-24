9e7d0f75810a791ec451b86223490fc95db41629
Hoishin <hoishinxii@gmail.com>
2018-03-25 00:46:10 +0900

    feat (javascript): add navbar

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+	<header class="navbar">
+		<section class="navbar-section">
+			<a href="/" class="navbar-brand mr-2">TODO</a>
+			<a href="/" class="btn btn-link">Home</a>
+			<a href="/list" class="btn btn-link">List</a>
+		</section>
+	</header>
+
+	<router-view/>
+</div>
 </template>
 
 <style>
+@import url("../node_modules/spectre.css");
+</style>
+
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+	margin: 10px;
 }
 </style>
