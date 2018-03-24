c20c3196ecd84128c9050b687be8273be8b62f7b
Hoishin <hoishinxii@gmail.com>
2018-03-25 00:57:48 +0900

    feat (javascript): add about page

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -4,6 +4,7 @@
 		<section class="navbar-section">
 			<a href="/" class="navbar-brand mr-2">TODO</a>
 			<a href="/" class="btn btn-link">Home</a>
+			<a href="/about" class="btn btn-link">About</a>
 			<a href="/list" class="btn btn-link">List</a>
 		</section>
 	</header>
