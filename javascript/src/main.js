bb1ad636db97571784135e164a5b3fea5e887a8a
Hoishin <hoishinxii@gmail.com>
2018-03-25 00:16:36 +0900

    feat (javascript): sync store with router

--- javascript/src/main.js
+++ javascript/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
+import {sync} from 'vuex-router-sync';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
 
+const unsync = sync(store, router);
+
 new Vue({
 	router,
 	store,
 	render: h => h(App),
 }).$mount('#app');
+
+unsync();
