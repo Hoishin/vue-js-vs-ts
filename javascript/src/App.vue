cea52e63d13328889f0be59328ffa7ae5d6cba8b
Hoishin <hoishinxii@gmail.com>
2018-03-25 01:37:17 +0900

    style (javascript): update XO config

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -2,26 +2,22 @@
 	<div id="app">
 		<header class="navbar">
 			<section class="navbar-section">
-				<a 
-					href="/" 
-					class="navbar-brand mr-2"
-				>TODO</a>
-				<a 
-					href="/" 
-					class="btn btn-link"
-				>Home</a>
-				<a 
-					href="/about" 
-					class="btn btn-link"
-				>About</a>
-				<a 
-					href="/list" 
-					class="btn btn-link"
-				>List</a>
+				<a
+					href="/"
+					class="navbar-brand mr-2">TODO</a>
+				<a
+					href="/"
+					class="btn btn-link">Home</a>
+				<a
+					href="/about"
+					class="btn btn-link">About</a>
+				<a
+					href="/list"
+					class="btn btn-link">List</a>
 			</section>
 		</header>
 
-		<router-view/>>
+		<router-view/>
 	</div>
 </template>
 
